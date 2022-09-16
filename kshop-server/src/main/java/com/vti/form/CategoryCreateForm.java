ecd1fbca329d9de8dab9a77eb43100c03fb2eb01
khoa-omega <nvkhoa05@gmail.com>
2022-09-16 21:15:43 +0700

    Update category forms

--- kshop-server/src/main/java/com/vti/form/CategoryCreateForm.java
+++ kshop-server/src/main/java/com/vti/form/CategoryCreateForm.java
@@ -1,4 +1,12 @@
 package com.vti.form;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CategoryCreateForm {
+    private String name;
 }
