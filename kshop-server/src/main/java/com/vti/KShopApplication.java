5a481e52ecc055034383ae3ce4058eb0cabebddf
khoa-omega <nvkhoa05@gmail.com>
2022-09-20 01:01:23 +0700

    Avoid cross origin

--- kshop-server/src/main/java/com/vti/KShopApplication.java
+++ kshop-server/src/main/java/com/vti/KShopApplication.java
@@ -2,6 +2,9 @@ package com.vti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KShopApplication {
@@ -10,4 +13,16 @@ public class KShopApplication {
 		SpringApplication.run(KShopApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOrigins("http://127.0.0.1:5500")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
